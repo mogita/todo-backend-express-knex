f48b3bf309b4e6561bb5d71336721977ce348bc9
mogita <me@mogita.com>
2025-03-30 22:55:34 +0800

    test: update todo's test cases to reflect the project scope

--- server/tests/util/httpRequests.ts
+++ server/tests/util/httpRequests.ts
@@ -1,37 +1,37 @@
-import request from 'supertest'
+import request, { Test } from 'supertest'
 import app from '../../server.ts'
 
 // a helper function to make a POST request.
-function post(url, body) {
+function post(url: string, body: any): Test {
   const httpRequest = request(app).post(url)
   httpRequest.send(body)
   httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set('Origin', `http://localhost:${process.env.PORT}`)
   return httpRequest
 }
 
 // a helper function to make a GET request.
-function get(url) {
+function get(url: string): Test {
   const httpRequest = request(app).get(url)
   httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set('Origin', `http://localhost:${process.env.PORT}`)
   return httpRequest
 }
 
 // a helper function to make a PATCH request.
-function patch(url, body) {
+function patch(url: string, body: any): Test {
   const httpRequest = request(app).patch(url)
   httpRequest.send(body)
   httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set('Origin', `http://localhost:${process.env.PORT}`)
   return httpRequest
 }
 
 // a helper function to make a DELETE request.
-function del(url) {
+function del(url: string): Test {
   const httpRequest = request(app).delete(url)
   httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set('Origin', `http://localhost:${process.env.PORT}`)
   return httpRequest
 }
 
