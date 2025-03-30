e9f80b8fce1b5701d9e169c27c8f9e0b542529eb
mogita <me@mogita.com>
2025-03-30 15:01:45 +0800

    style: format server codes with prettier

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
-const app = require('./server-config.js');
-const routes = require('./server-routes.js');
+const app = require('./server-config.js')
+const routes = require('./server-routes.js')
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.get('/', routes.getAllTodos);
-app.get('/:id', routes.getTodo);
+app.get('/', routes.getAllTodos)
+app.get('/:id', routes.getTodo)
 
-app.post('/', routes.postTodo);
-app.patch('/:id', routes.patchTodo);
+app.post('/', routes.postTodo)
+app.patch('/:id', routes.patchTodo)
 
-app.delete('/', routes.deleteAllTodos);
-app.delete('/:id', routes.deleteTodo);
+app.delete('/', routes.deleteAllTodos)
+app.delete('/:id', routes.deleteTodo)
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(port, () => console.log(`Listening on port ${port}`));
+  app.listen(port, () => console.log(`Listening on port ${port}`))
 }
 
-module.exports = app;
+module.exports = app
+
