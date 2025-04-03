4bb532c694e785833a557ebdb1692e37378f3d4c
mogita <me@mogita.com>
2025-04-03 19:35:45 +0800

    feat: add access logs

--- server/server-config.ts
+++ server/server-config.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import morgan from 'morgan'
 
 const app = express()
 
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('dev'))
+}
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -10,7 +14,7 @@ app.use(
   }),
 )
 
-app.use(function (req, res, next) {
+app.use(function (_, res, next) {
   res.header('Access-Control-Allow-Headers', 'Content-Type')
   res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE')
   res.header('Access-Control-Allow-Origin', '*')
